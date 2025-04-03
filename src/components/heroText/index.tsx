b02e39d8caeb348663768923339bfdb48c27e848
JAYANT ISSAR <jayantissar23@gmail.com>
2025-04-03 05:53:01 +0530

    added dark mode styling

--- src/components/heroText/index.tsx
+++ src/components/heroText/index.tsx
@@ -11,12 +11,17 @@ const HeroText = () => {
                 Sign-up for our 30-day trail. No credit card required.
             </h5>
         </div>
-        <svg 
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 z-0 pointer-events-none"
-            xmlns="http://www.w3.org/2000/svg" 
-            width="146" 
-            height="145"
-        ><g fill="none" fillRule="evenodd" stroke="#CFD8EF"><circle cx="63" cy="82" r="62.5"/><circle cx="105" cy="41" r="40.5"/></g></svg>
+        <svg
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 z-0 pointer-events-none"
+        xmlns="http://www.w3.org/2000/svg"
+        width="146"
+        height="145"
+        >
+        <g fill="none" fillRule="evenodd" className="stroke-[#CFD8EF] dark:stroke-[#2A3344]">
+            <circle cx="63" cy="82" r="62.5"/>
+            <circle cx="105" cy="41" r="40.5"/>
+        </g>
+        </svg>
     </div>
     
   )
