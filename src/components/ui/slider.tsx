26a0b7a5d68ad3fba0c56710b6ade12aacb1b57b
JAYANT ISSAR <jayantissar23@gmail.com>
2025-04-03 06:20:55 +0530

    added active-state bg to slider thumbh

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -53,7 +53,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className=" border-primary flex justify-center items-center bg-[color:hsl(174,86%,45%)] ring-ring/50 data-[state=active]:bg-[color:hsl(174,86%,45%)] size-8 shrink-0 rounded-full transition-[color,box-shadow] in-focus:cursor-pointer hover:cursor-pointer focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 active:cursor-grabbing
+          className=" border-primary flex justify-center items-center bg-[color:hsl(174,86%,45%)] ring-ring/50 data-[state=active]:bg-[color:hsl(174,86%,45%)] size-8 shrink-0 rounded-full transition-[color,box-shadow] in-focus:cursor-pointer hover:cursor-pointer focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 active:cursor-grabbing active:bg-[color:hsl(174,86%,35%)]
           shadow-[0_12px_24px_-2px_rgba(16,185,129,0.5)] hover:shadow-[0_20px_24px_-2px_rgba(16,185,129,0.6)]
           "
         >
